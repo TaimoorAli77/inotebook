c12a457199390fa0201a439e9a00a206ce38063a
taimoor <taimoorali083@gmail.com>
2023-04-01 15:02:38 +0500

    "Now data can be fetched from jwt token"

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-
+const user = require("./User");
 const NotesSchema = new Schema({
-name:{
+user:{
+type:mongoose.Schema.Types.ObjectId,
+ref:user
+},
+title:{
     type:String,
     required:true
 },
